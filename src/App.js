e257c857f618496b240bcfe7a56b3db5837630a8
Hussain <hussainabuwala.1997@gmail.com>
2023-07-09 11:47:01 -0400

    added Game component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import Game from './components/Board';
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Game />
     </div>
   );
 }
